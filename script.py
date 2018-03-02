d4c0259f352a7a40682bf7639606f88cecb570c3
Borobov Pavel <blvp.me@gmail.com>
2018-03-02 18:39:10 +0300

    added model and preprocessing

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import re
+from collections import namedtuple
+from typing import NamedTuple
 
 import numpy as np
 import pandas as pd
@@ -6,6 +8,7 @@ import tflearn
 from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 
 def cleaned(content):
@@ -21,12 +24,12 @@ def cleaned(content):
     cleaned_content = re.sub("''|,", "", cleaned_content)
     cleaned_content = re.sub(r" {2}", " ", cleaned_content)
     cleaned_content = re.sub(r"[^A-Za-z0-9(),!?\'`]", " ", cleaned_content)
-    cleaned_content = re.sub(r"\'s", " \'s", cleaned_content)
-    cleaned_content = re.sub(r"\'ve", " \'ve", cleaned_content)
-    cleaned_content = re.sub(r"n\'t", " n\'t", cleaned_content)
-    cleaned_content = re.sub(r"\'re", " \'re", cleaned_content)
-    cleaned_content = re.sub(r"\'d", " \'d", cleaned_content)
-    cleaned_content = re.sub(r"\'ll", " \'ll", cleaned_content)
+    cleaned_content = re.sub(r"\'s", " is", cleaned_content)
+    cleaned_content = re.sub(r"\'ve", " have", cleaned_content)
+    cleaned_content = re.sub(r"n\'t", " not", cleaned_content)
+    cleaned_content = re.sub(r"\'re", " are", cleaned_content)
+    cleaned_content = re.sub(r"\'d", " would", cleaned_content)
+    cleaned_content = re.sub(r"\'ll", " will", cleaned_content)
     cleaned_content = re.sub(r",", " , ", cleaned_content)
     cleaned_content = re.sub(r"!", " ! ", cleaned_content)
     cleaned_content = re.sub(r"\(", " \( ", cleaned_content)
@@ -47,23 +50,46 @@ def preprocess(doc):
 
 
 def get_network(vocab_len, n_classes):
-    input = tflearn.input_data([vocab_len])
-    return tflearn.fully_connected(input, n_classes, weights_init='xavier')
+    input = tflearn.input_data([None, vocab_len])
+    net = tflearn.fully_connected(input, n_classes, weights_init='xavier')
+    return tflearn.regression(net, loss='binary_crossentropy', n_classes=n_classes)
 
 
-def main():
+DataSet = namedtuple('DataSet', ['x', 'y'])
+
+
+def load_data():
     # comment_text, labels...
-    df = pd.read_csv('data/train.csv')
+    df = pd.read_csv('data/train.csv')[:10000]
     tf_idf = TfidfVectorizer(
         stop_words='english',
         tokenizer=space_tokenizer,
         preprocessor=preprocess,
         sublinear_tf=True,
-        use_idf=False
+        use_idf=False,
+        lowercase=True
     )
-    nclasses = 6
     documents = tf_idf.fit_transform(df['comment_text'])
-    net = get_network(len(tf_idf.vocabulary_))
+    labels = df.drop(['id', 'comment_text'], axis=1)
+    # train_x, test_x, train_y, test_y = train_test_split(documents, labels, test_size=0.3)
+    return DataSet(np.array(documents.toarray()), np.array(labels)), len(tf_idf.vocabulary_)
+
+
+def main():
+    print('loading data')
+    dataset, vocab_size = load_data()
+    nclasses = 6
+    net = get_network(vocab_size, nclasses)
+    model = tflearn.DNN(net, tensorboard_verbose=1, tensorboard_dir='logs/experiment')
+    print('model ready. start fitting the model')
+    model.fit(dataset.x, dataset.y,
+              n_epoch=10,
+              validation_set=0.3,
+              show_metric=True,
+              batch_size=64,
+              shuffle=True,
+              run_id='bow_logits'
+              )
 
 
 if __name__ == '__main__':
